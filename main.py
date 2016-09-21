f3b11b8c989782088446f2381fd89252b71c9ca4
nikhilrampal15 <nikhilrampal15@gmail.com>
2016-09-20 22:34:49 -0700

    scraping from yahoo now

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 """
 Scrapes Ticker symbol of every mutual fund available
 """
-import bs4
-import requests
-base_url = 'http://www.marketwatch.com/tools/mutual-fund/list?firstLetter=A'
-response = requests.get(base_url)
-soup = bs4.BeautifulSoup(response.text, "html.parser")
-for x in soup.findAll("td", class_="quotelist-symb"):
-    print(x.string)
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+#conservative allocation
+url = 'http://finance.yahoo.com/funds/lists/?bypass=true&mod_id=mediaquotesmutualfunds&scol=nav3m&stype=desc&rcnt=100&tab=tab1&cat=%24FOCA%24CA%24%24&page=1'
+driver.get(url)
+html = driver.page_source
+driver.quit()
+soup = BeautifulSoup(html)
+
+for tag in soup.find_all('td', class_='tkr'):
+    print (tag.string)
