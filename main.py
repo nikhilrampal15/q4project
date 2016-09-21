2a3bf954bc9198c9291362e42fd1c8002c3a35e6
Nikhil <nikhilrampal15@gmail.com>
2016-09-20 17:28:07 -0700

    ticker symbol scraping

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import quandl
-mydata = quandl.get("FRED/GDP")
-data = quandl.get("WIKI/FB")
-print(data)
+"""
+Scrapes Ticker symbol of every mutual fund available
+"""
+import bs4
+import requests
+base_url = 'http://www.marketwatch.com/tools/mutual-fund/list?firstLetter=A'
+response = requests.get(base_url)
+soup = bs4.BeautifulSoup(response.text, "html.parser")
+for x in soup.findAll("td", class_="quotelist-symb"):
+    print(x.string)
