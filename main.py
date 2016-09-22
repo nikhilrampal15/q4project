7de57ad2e86b0fecd1cb2b32d89cdeb364409c91
Nikhil <nikhilrampal15@gmail.com>
2016-09-22 09:51:38 -0700

    got ticker information for mutual funds in US

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ Scrapes Ticker symbol of every mutual fund available
 """
 import bs4
 import requests
-base_url = 'http://www.marketwatch.com/tools/mutual-fund/list?firstLetter=A'
-response = requests.get(base_url)
-soup = bs4.BeautifulSoup(response.text, "html.parser")
+import string
+import csv
+
 tickers = []
-for x in soup.findAll("td", class_="quotelist-symb"):
-    tickers.append(x.string)
-print(tickers)
+alphabet_arr = list(string.ascii_uppercase)
+for i in alphabet_arr:
+    base_url = 'http://www.marketwatch.com/tools/mutual-fund/list?firstLetter={}'.format(i)
+    response = requests.get(base_url)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    tickers = []
+    for j in soup.findAll("td", class_="quotelist-symb"):
+        tickers.append(j.string)
+
+
+csv_file = "./tickers.csv"
+
+with open(csv_file, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for symbol in tickers:
+        writer.writerow([symbol])
